645597ea3b93bb0f024eefc5eae6bc4e606b16e1
Perska <perska.weegee@gmail.com>
2020-12-07 07:41:42 +0200

    added day 7 and moved the day programs to their own folder

---
+++ AoC2020/Days/Day7.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2020
+{
+	partial class Program
+	{
+		static void Day7(List<string> input)
+		{
+			Dictionary<string, List<string>> rules = new Dictionary<string, List<string>>();
+			foreach (string rule in input)
+			{
+				if (rule == "") continue;
+				string[] part = rule.Replace(".", "").Split(new string[] { " bags contain " }, StringSplitOptions.None);
+				rules[part[0]] = part[1].Replace("bags", "bag").Replace(" bag", "").Split(new string[] { ", " }, StringSplitOptions.None).ToList();
+			}
+
+			int count = 0;
+			foreach (var item in rules)
+			{
+				if (BagContains(rules, item.Key, "shiny gold")) count++;
+			}
+			Console.WriteLine(count);
+			Console.WriteLine(CountBags(rules,"shiny gold"));
+		}
+
+		static bool BagContains(Dictionary<string, List<string>> rules, string bag, string targetBag)
+		{
+			if (rules[bag].Where(item => item.Contains(targetBag)).Count() != 0) return true;
+			foreach (var item in rules[bag])
+			{
+				if (item == "no other") continue;
+				if (BagContains(rules, item.Substring(2), targetBag)) return true;
+			}
+			return false;
+		}
+
+		static int CountBags(Dictionary<string, List<string>> rules, string bag)
+		{
+			int bags = 0;
+			foreach (var item in rules[bag])
+			{
+				if (item == "no other") continue;
+				int bagM = item[0] - '0';
+				bags+=bagM;
+				for (int i = 0; i < bagM; i++)
+				{
+					bags += CountBags(rules, item.Substring(2));
+				}
+			}
+			return bags;
+		}
+	}
+}
--- AoC2020/Program.cs
+++ AoC2020/Program.cs
@@ -12,7 +12,7 @@ namespace AoC2020
 
 		public static List<DayProgram> days = new List<DayProgram>
 		{
-			Day1, Day2, Day3, Day4, Day5, Day6
+			Day1, Day2, Day3, Day4, Day5, Day6, Day7
 		};
 
 		static void Main()
