eaf76a59d68d21f71ae790713f38f9c34552856a
Perska <perska.weegee@gmail.com>
2020-12-21 18:42:53 +0200

    add day 21 and SuperReadLine

---
+++ AoC2020/Days/Day21.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AoC2020
+{
+	partial class Program
+	{
+		static void Day21(List<string> input)
+		{
+			List<string> ingredients = new List<string>();
+			List<string> allergens = new List<string>();
+			Dictionary<string, int> allergenCount = new Dictionary<string, int>();
+			Dictionary<string, Dictionary<string, int>> listings = new Dictionary<string, Dictionary<string, int>>();
+			Regex regex = new Regex(@"(.*) \(contains (.*)\)", RegexOptions.Compiled | RegexOptions.ECMAScript);
+			foreach (var item in input)
+			{
+				Match match = regex.Match(item);
+				if (match.Success)
+				{
+					var ingredientPart = match.Groups[1].Value.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+					var allergenPart = match.Groups[2].Value.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+					foreach (var allerg in allergenPart)
+					{
+						foreach (var ingred in ingredientPart)
+						{
+							if (!listings.ContainsKey(ingred)) listings[ingred] = new Dictionary<string, int>();
+							if (!listings[ingred].ContainsKey(allerg)) listings[ingred][allerg] = 0;
+							listings[ingred][allerg]++;
+						}
+						if (!allergenCount.ContainsKey(allerg)) allergenCount[allerg] = 0;
+						allergenCount[allerg]++;
+					}
+					ingredients.AddRange(ingredientPart);
+					allergens.AddRange(allergenPart);
+				}
+			}
+			//ingredients = ingredients.Distinct().ToList();
+			//allergens =     allergens.Distinct().ToList();
+
+			Dictionary<string, string> ingredientAllergen = new Dictionary<string, string>();
+			//var l = listings.ToList().OrderBy(item => item.Value.Count(sub => sub.Value == allergenCount[sub.Key]));
+
+			while (true)
+			{
+				var singleField = listings.Where(item => item.Value.Count(ok => ok.Value == allergenCount[ok.Key]) == 1);
+				if (singleField.Count() == 0) break;
+				foreach (var rule in singleField)
+				{
+					/*foreach (var item in listings)
+					{
+						foreach (var item2 in item.Value)
+						{
+							if (item2.Value==true) allerg = item.Key
+						}
+					}*/
+					string allerg = rule.Value.First(item => item.Value == allergenCount[item.Key]).Key;
+					ingredientAllergen[rule.Key] = allerg;
+					foreach (var rule2 in listings)
+					{
+						rule2.Value[allerg] = 0;
+					}
+				}
+			}
+			int count = 0;
+			foreach (var item in ingredients)
+			{
+				if (!ingredientAllergen.ContainsKey(item)) count++;
+			}
+			Console.WriteLine($"Part 1: {count}");
+			string danger = "";
+			foreach (var item in ingredientAllergen.OrderBy(item => item.Value))
+			{
+				danger += item.Key + ",";
+			}
+			danger = danger.Substring(0, danger.Length - 1);
+			Console.WriteLine(danger);
+			;
+		}
+	}
+}
--- AoC2020/Program.cs
+++ AoC2020/Program.cs
@@ -14,14 +14,14 @@ namespace AoC2020
 		{
 			Day01, Day02, Day03, Day04, Day05, Day06, Day07,
 			Day08, Day09, Day10, Day11, Day12, Day13, Day14,
-			Day15, Day16, Day17, Day18, Day19, Day20
+			Day15, Day16, Day17, Day18, Day19, Day20, Day21
 		};
 
 		static void Main()
 		{
 			bool keepGoing = true;
 
-
+			//Console.WriteLine(SuperReadLine());
 			while (keepGoing)
 			{
 				Console.WriteLine("Enter the day you want to run the program for (enter 0 to stop)");
@@ -47,7 +47,7 @@ namespace AoC2020
 						Console.WriteLine("Please enter the program input. Once done, enter \"end\"\n(hint, right-click the window top bar for pasting)");
 						while (true)
 						{
-							string line = Console.ReadLine();
+							string line = day == 21 ? SuperReadLine() : Console.ReadLine();
 							if (line.ToLowerInvariant().StartsWith("end")) break;
 							input.Add(line);
 						}
@@ -65,6 +65,37 @@ namespace AoC2020
 				}
 				Console.WriteLine();
 			}
+		}
+
+		static string SuperReadLine()
+		{
+			char key;
+			StringBuilder input = new StringBuilder();
+			int x = Console.CursorLeft, y = Console.CursorTop;
+			while ((key = Console.ReadKey(true).KeyChar) != 13)
+			{
+				if (key == 8 && input.Length > 0)
+				{
+					input.Remove(input.Length - 1, 1);
+				}
+				else if (key != 8)
+				{
+					input.Append(key);
+				}
+				if (key == 8)
+				{
+					Console.SetCursorPosition((x + Math.Max(input.Length - 1, 0)) % Console.BufferWidth, y + (x + Math.Max(input.Length - 1, 0)) / Console.BufferWidth);
+					Console.Write(input.ToString().LastOrDefault().ToString() + '\0');
+					Console.SetCursorPosition((x + input.Length) % Console.BufferWidth, y + (x + input.Length) / Console.BufferWidth);
+				}
+				else
+				{
+					Console.Write(key);
+					//Console.SetCursorPosition((x + input.Length) % Console.BufferWidth, y + (x + input.Length) / Console.BufferWidth);
+				}
+			}
+			Console.SetCursorPosition(0, y + (x + input.Length) / Console.BufferWidth + 1);
+			return input.ToString();
 		}
 	}
 }
