9f2509482f258f96e0b132aeb84406477c95b425
Perska <perska.weegee@gmail.com>
2020-12-24 04:37:26 +0200

    small optimization on day 23
    removed isIn dictionary from use
    now the node location table is consulted
    (if removed, the number's node is null)
    also pre-emptively added days 24's and 25's code files

--- AoC2020/Days/Day23.cs
+++ AoC2020/Days/Day23.cs
@@ -14,26 +14,21 @@ namespace AoC2020
 			LinkedList<int> cups2 = new LinkedList<int>();
 			Dictionary<int, LinkedListNode<int>> nodeLoc = new Dictionary<int, LinkedListNode<int>>();
 			Dictionary<int, LinkedListNode<int>> nodeLoc2 = new Dictionary<int, LinkedListNode<int>>();
-			Dictionary<int, bool> isIn = new Dictionary<int, bool>();
-			Dictionary<int, bool> isIn2 = new Dictionary<int, bool>();
 			for (int i = 0; i < input[0].Length; i++)
 			{
 				int num = int.Parse(input[0][i].ToString());
 				nodeLoc[num] = cups.AddLast(num);
 				nodeLoc2[num] = cups2.AddLast(num);
-				isIn[num] = true;
-				isIn2[num] = true;
 			}
 			int moreCup = cups.Max() + 1;
 			while (moreCup <= 1000000)
 			{
 				nodeLoc2[moreCup] = cups2.AddLast(moreCup);
-				isIn2[moreCup] = true;
 				moreCup++;
 			}
 			var thing = cups2.Reverse().ToList();
 
-			ShuffleCups(cups, nodeLoc, isIn, 100);
+			ShuffleCups(cups, nodeLoc, 100);
 
 			
 			LinkedListNode<int> index = nodeLoc[1];
@@ -44,19 +39,18 @@ namespace AoC2020
 			}
 			Console.WriteLine();
 
-			ShuffleCups(cups2, nodeLoc2, isIn2, 10000000);
+			ShuffleCups(cups2, nodeLoc2, 10000000);
 			index = nodeLoc2[1];
 			long product = 1;
 			for (int i = 0; i < 2; i++)
 			{
 				index = index.Next ?? cups.First;
-				Console.WriteLine(index.Value);
 				product *= index.Value;
 			}
 			Console.WriteLine(product);
 		}
 
-		static void ShuffleCups(LinkedList<int> cups, Dictionary<int, LinkedListNode<int>> nodeLoc, Dictionary<int, bool> isIn, int moves)
+		static void ShuffleCups(LinkedList<int> cups, Dictionary<int, LinkedListNode<int>> nodeLoc, int moves)
 		{
 			LinkedListNode<int> index = cups.First;
 			Stack<int> removed = new Stack<int>();
@@ -69,11 +63,15 @@ namespace AoC2020
 				{
 					int num = (index.Next ?? cups.First).Value;
 					removed.Push(num);
-					isIn.Remove(num);
+					nodeLoc[num] = null;
 					cups.Remove(index.Next ?? cups.First);
 				}
 				int target = currentCup - 1;
-				while (removed.Contains(target) || !isIn.TryGetValue(target, out bool res))
+				if (target < 1)
+				{
+					target = isInMax;
+				}
+				while (nodeLoc[target] == null)
 				{
 					target--;
 					if (target < 1)
@@ -86,7 +84,6 @@ namespace AoC2020
 				{
 					int num = removed.Pop();
 					nodeLoc[num] = cups.AddAfter(index2, num);
-					isIn[num] = true;
 				}
 				index = index.Next ?? cups.First;
 			}
---
+++ AoC2020/Days/Day25.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2020
+{
+	partial class Program
+	{
+		static void Day25(List<string> input)
+		{
+
+		}
+	}
+}
