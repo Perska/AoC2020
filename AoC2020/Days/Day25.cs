6476ae76e2280268ff3963e9cfc57c77715a88fb
Perska <perska.weegee@gmail.com>
2020-12-25 07:29:24 +0200

    added day 25

--- AoC2020/Days/Day25.cs
+++ AoC2020/Days/Day25.cs
@@ -10,7 +10,42 @@ namespace AoC2020
 	{
 		static void Day25(List<string> input)
 		{
+			if (input.Count < 2)
+			{
+				Console.WriteLine("Input too short!");
+				return;
+			}
+			int cardKey = int.Parse(input[0]);
+			int doorKey = int.Parse(input[1]);
 
+			int cardLoopSize = UnTransformSubject(7, cardKey);
+			int doorLoopSize = UnTransformSubject(7, doorKey);
+
+			long encKey = TransformSubject(doorKey, cardLoopSize);
+			Console.WriteLine(encKey);
+
+		}
+		static long TransformSubject(long subject, int loopSize)
+		{
+			long value = 1;
+			for (int i = 0; i < loopSize; i++)
+			{
+				value *= subject;
+				value %= 20201227;
+			}
+			return value;
+		}
+		static int UnTransformSubject(long subject, int target)
+		{
+			long value = 1;
+			int loops = 0;
+			while (value != target)
+			{
+				value *= subject;
+				value %= 20201227;
+				loops++;
+			}
+			return loops;
 		}
 	}
 }
